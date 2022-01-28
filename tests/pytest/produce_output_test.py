05d303880c468fe2d14ae264dfa1b315537415ea
Paola Ferrario <paola.ferrario@gmail.com>
2022-01-28 15:48:23 +0100

    Eliminate verbosity running tests.

--- tests/pytest/produce_output_test.py
+++ tests/pytest/produce_output_test.py
@@ -40,6 +40,8 @@ def test_create_nexus_output_file_full_body(config_tmpdir, output_tmpdir, PETALO
 /event/verbose 0
 /tracking/verbose 0
 
+/process/em/verbose 0
+
 /Geometry/FullRingInfinity/depth 3. cm
 /Geometry/FullRingInfinity/pitch 7. mm
 /Geometry/FullRingInfinity/inner_radius 380. mm
@@ -107,6 +109,8 @@ def test_create_petalo_output_file_ring_tiles(config_tmpdir, output_tmpdir, PETA
 /run/verbose 1
 /event/verbose 0
 /tracking/verbose 0
+
+/process/em/verbose 0
 
 /Geometry/FullRingTiles/depth 3. cm
 /Geometry/FullRingTiles/inner_radius 165. mm
@@ -176,6 +180,8 @@ def test_create_petalo_output_file_pet_box_all_tiles(config_tmpdir, output_tmpdi
 /run/verbose 1
 /event/verbose 0
 /tracking/verbose 0
+
+/process/em/verbose 0
 
 /Geometry/PetBox/tile_type_d {tile_type1}
 /Geometry/PetBox/tile_type_c {tile_type2}
