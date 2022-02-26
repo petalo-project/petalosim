1237abc1e8dc7f6cf06716bc097257abf62e9514
Paola Ferrario <paola.ferrario@gmail.com>
2022-02-26 15:00:49 +0100

    Improve the documentation of the function

--- source/utils/PetaloUtils.h
+++ source/utils/PetaloUtils.h
@@ -12,7 +12,8 @@
 #ifndef PET_UTILS_H
 #define PET_UTILS_H
 
-// Returns a unit vector in a direction at a given angle from a given direction
+// Given an angle and a vector, the function returns a unitary vector whose
+// scalar product with the first vector is equal to the cosine of the angle
 G4ThreeVector GenerateNonCollinearVector(G4ThreeVector dir, G4double angle);
 
 // Returns the momentum of the second gamma and the energies of both gammas
