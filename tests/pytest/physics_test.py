8070a7e40a5d6aaf6fa05558235422d69e9fa955
Paola Ferrario <paola.ferrario@gmail.com>
2022-06-02 13:01:07 +0200

    Add check for thermalelectrons

--- tests/pytest/physics_test.py
+++ tests/pytest/physics_test.py
@@ -3,13 +3,14 @@ import os
 
 import pandas as pd
 
-def test_opt_photons_are_produced_by_nest(file_name_nest):
+def test_opt_photons_and_ioni_elec_are_produced_by_nest(file_name_nest):
     """Check that optical photons come from the NEST process, called 'S1'"""
 
-    particles   = pd.read_hdf(file_name_nest, 'MC/particles')
-    opt_photons = particles[particles.particle_name == 'opticalphoton']
+    particles = pd.read_hdf(file_name_nest, 'MC/particles')
+    selection = (particles.particle_name == 'opticalphoton') | (particles.particle_name == 'thermalelectron') 
+    opt_photons_and_elec = particles[selection]
 
-    creator_processes = opt_photons.creator_proc.unique()
+    creator_processes = opt_photons_and_elec.creator_proc.unique()
     
     assert creator_processes == ['S1']
     
