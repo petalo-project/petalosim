0410eae42dd72eefef84467bdcf276de9824421e
Paola Ferrario <paola.ferrario@gmail.com>
2023-02-01 15:45:18 +0100

    Remove unnecessary namespace

--- source/actions/PetAnalysisRunAction.cc
+++ source/actions/PetAnalysisRunAction.cc
@@ -16,9 +16,6 @@
 #include <G4Run.hh>
 #include <G4AnalysisManager.hh>
 
-namespace nexus {}
-using namespace nexus;
-
 REGISTER_CLASS(PetAnalysisRunAction, G4UserRunAction)
 
 PetAnalysisRunAction::PetAnalysisRunAction(): G4UserRunAction()
--- source/actions/PetSaveAllSteppingAction.cc
+++ source/actions/PetSaveAllSteppingAction.cc
@@ -17,9 +17,6 @@
 #include <G4ProcessManager.hh>
 #include <G4ParticleTable.hh>
 
-namespace nexus {}
-using namespace nexus;
-
 REGISTER_CLASS(PetSaveAllSteppingAction, G4UserSteppingAction)
 
 PetSaveAllSteppingAction::PetSaveAllSteppingAction():
