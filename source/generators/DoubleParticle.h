23719c61964757c7e167ce2a6a4cf26c821f8e98
Paola Ferrario <paola.ferrario@gmail.com>
2023-02-02 17:22:45 +0100

    Remove commented line and fix indentation

--- source/generators/DoubleParticle.h
+++ source/generators/DoubleParticle.h
@@ -20,42 +20,39 @@ class G4ParticleDefinition;
 
 namespace nexus { class GeometryBase; }
 
-  class DoubleParticle: public G4VPrimaryGenerator
-  {
-  public:
-    /// Constructor
-    DoubleParticle();
-    /// Destructor
-    ~DoubleParticle();
-
-    /// This method is invoked at the beginning of the event. It sets
-    /// a primary vertex (that is, a particle in a given position and time)
-    /// in the event.
-    void GeneratePrimaryVertex(G4Event*);
-
-  private:
-
-    void SetParticleDefinition(G4String);
-
-    /// Generate a random kinetic energy with flat probability in
-    //  the interval [energy_min, max_energy].
-    G4double RandomEnergy() const;
-
-  private:
-    G4GenericMessenger* msg_;
-
-    G4ParticleDefinition* particle_definition_;
-
-    G4double energy_min_; ///< Minimum kinetic energy
-    G4double max_energy_; ///< Maximum kinetic energy
-
-    const nexus::GeometryBase* geom_; ///< Pointer to the detector geometry
-
-    G4String region_;
-
-
-  };
-
-//} // end namespace nexus
-
+class DoubleParticle: public G4VPrimaryGenerator
+{
+public:
+  /// Constructor
+  DoubleParticle();
+  /// Destructor
+  ~DoubleParticle();
+  
+  /// This method is invoked at the beginning of the event. It sets
+  /// a primary vertex (that is, a particle in a given position and time)
+  /// in the event.
+  void GeneratePrimaryVertex(G4Event*);
+  
+private:
+  
+  void SetParticleDefinition(G4String);
+  
+  /// Generate a random kinetic energy with flat probability in
+  //  the interval [energy_min, max_energy].
+  G4double RandomEnergy() const;
+  
+private:
+  G4GenericMessenger* msg_;
+  
+  G4ParticleDefinition* particle_definition_;
+  
+  G4double energy_min_; ///< Minimum kinetic energy
+  G4double max_energy_; ///< Maximum kinetic energy
+  
+  const nexus::GeometryBase* geom_; ///< Pointer to the detector geometry
+  
+  G4String region_;
+  
+  
+};
 #endif
